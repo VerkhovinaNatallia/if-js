c5dcaabdb6d7d06036a4ea918628d3b048e7d506
VerkhovinaNatallia <natalliaverkhovina@gmail.com>
2023-09-17 00:20:48 +0300

    Did the task 5

--- scripts/modules.js
+++ scripts/modules.js
@@ -1,24 +1,63 @@
-const text1=document.getElementById('text1');
-const text2=document.getElementById('text2');
-const text3=document.getElementById('text3');
-const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+const date = '2020-11-26';
+const regular = new RegExp('([0-9]+)[-]([0-9]+)[-]([0-9]+)');
+const arr_data=date.match(regular);
+console.log(`${arr_data[3]}.${arr_data[2]}.${arr_data[1]}`);
 
-function сhangesСolor() {
-    for (let i = 0; i <= colors.length; ) {
-        return function (event) {
-            event.target.style.color = colors[i];
-            i++;
-            if (i >= colors.length) {
-                i = 0;
-            }
-        };
+
+
+
+const data = [
+    {
+        country: 'Russia',
+        city: 'Saint Petersburg',
+        hotel: 'Hotel Leopold',
+    },
+    {
+        country: 'Spain',
+        city: 'Santa Cruz de Tenerife',
+        hotel: 'Apartment Sunshine',
+    },
+    {
+        country: 'Slowakia',
+        city: 'Vysokie Tatry',
+        hotel: 'Villa Kunerad',
+    },
+    {
+        country: 'Germany',
+        city: 'Berlin',
+        hotel: 'Hostel Friendship',
+    },
+    {
+        country: 'Indonesia',
+        city: 'Bali',
+        hotel: 'Ubud Bali Resort&SPA',
+    },
+    {
+        country: 'Netherlands',
+        city: 'Rotterdam',
+        hotel: 'King Kong Hostel',
+    },
+    {
+        country: 'Marocco',
+        city: 'Ourika',
+        hotel: 'Rokoko Hotel',
+    },
+    {
+        country: 'Germany',
+        city: 'Berlin',
+        hotel: 'Hotel Rehberge Berlin Mitte',
+    },
+];
+const mas=[];
+
+function searchObjects( str){
+    for (let i=0;i<data.length;i++){
+        if(data[i].country==str || data[i].city==str || data[i].hotel==str ){
+            const data_new  = data[i].country +','+data[i].city +','+data[i].hotel;
+            mas.push(data_new);
+
+        }
     }
-};
-
-
-text1.addEventListener('click',сhangesСolor());
-text2.addEventListener('click',сhangesСolor());
-text3.addEventListener('click',сhangesСolor());
-            
-
+    console.log(mas);
+}
 
