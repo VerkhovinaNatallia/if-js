75423fe9e1f0ba6eca9edb39e5d5e4326edc7491
VerkhovinaNatallia <natalliaverkhovina@gmail.com>
2023-09-16 21:11:13 +0300

    Correction of a task with text

--- scripts/modules.js
+++ scripts/modules.js
@@ -3,46 +3,31 @@ const text2=document.getElementById('text2');
 const text3=document.getElementById('text3');
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
 
+function col() {
+    for (let i = 0; i <= colors.length; ) {
+        return function (event) {
+            event.target.style.color = colors[i];
+            i++;
+            if (i >= colors.length) {
+                i = 0;
+            }
+        };
+    }
+};
+
+
 text1.addEventListener(
     'click',
-    (function () {
-        for (let i = 0; i <= colors.length; ) {
-            return function (event) {
-                event.target.style.color = colors[i];
-                i++;
-                if (i >= colors.length) {
-                    i = 0;
-                }
-            };
-        }
-    })());
-
-
+    col()
+    );
 text2.addEventListener(
     'click',
-    (function () {
-        for (let i = 0; i <= colors.length; ) {
-            return function (event) {
-                event.target.style.color = colors[i];
-                i++;
-                if (i >= colors.length) {
-                    i = 0;
-                }
-            };
-        }
-    })());
-
+    col()
+    );
 text3.addEventListener(
-    'click',
-    (function () {
-        for (let i = 0; i <= colors.length; ) {
-            return function (event) {
-                event.target.style.color = colors[i];
-                i++;
-                if (i >= colors.length) {
-                    i = 0;
-                }
-            };
-        }
-    })());
+        'click',
+        col()
+        );
+            
+
 
