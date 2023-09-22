a3d4714608b4f238cb51158830444e2b0012f44f
VerkhovinaNatallia <natalliaverkhovina@gmail.com>
2023-09-22 14:03:01 +0300

    task 6

--- scripts/modules.js
+++ scripts/modules.js
@@ -1,64 +1,216 @@
 
-function flipDate(date){
-    const arr_data = date.split('-');
-    return (`${arr_data[2]}.${arr_data[1]}.${arr_data[0]}`);
-}
+const palindrome= (str)=>{return(str == str.split('').reverse().join(''))}
 
 
+const hotels = [
+    {
+      name: 'Hotel Leopold',
+      city: 'Saint Petersburg',
+      country: 'Russia',
+    },
+    {
+      name: 'Apartment Sunshine',
+      city: 'Santa Cruz de Tenerife',
+      country: 'Spain',
+    },
+    {
+      name: 'Villa Kunerad',
+      city: 'Vysokie Tatry',
+      country: 'Slowakia',
+    },
+    {
+      name: 'Hostel Friendship',
+      city: 'Berlin',
+      country: 'Germany',
+    },
+    {
+      name: 'Radisson Blu Hotel',
+      city: 'Kyiv',
+      country: 'Ukraine',
+    },
+    {
+      name: 'Paradise Hotel',
+      city: 'Guadalupe',
+      country: 'Mexico',
+    },
+    {
+      name: 'Hotel Grindewald',
+      city: 'Interlaken',
+      country: 'Switzerland',
+    },
+    {
+      name: 'The Andaman Resort',
+      city: 'Port Dickson',
+      country: 'Malaysia',
+    },
+    {
+      name: 'Virgin Hotel',
+      city: 'Chicago',
+      country: 'USA',
+    },
+    {
+      name: 'Grand Beach Resort',
+      city: 'Dubai',
+      country: 'United Arab Emirates',
+    },
+    {
+      name: 'Shilla Stay',
+      city: 'Seoul',
+      country: 'South Korea',
+    },
+    {
+      name: 'San Firenze Suites',
+      city: 'Florence',
+      country: 'Italy',
+    },
+    {
+      name: 'The Andaman Resort',
+      city: 'Port Dickson',
+      country: 'Malaysia',
+    },
+    {
+      name: 'Black Penny Villas',
+      city: 'BTDC, Nuca Dua',
+      country: 'Indonesia',
+    },
+    {
+      name: 'Koko Hotel',
+      city: 'Tokyo',
+      country: 'Japan',
+    },
+    {
+      name: 'Ramada Plaza',
+      city: 'Istanbul',
+      country: 'Turkey',
+    },
+    {
+      name: 'Waikiki Resort Hotel',
+      city: 'Hawaii',
+      country: 'USA',
+    },
+    {
+      name: 'Puro Hotel',
+      city: 'Krakow',
+      country: 'Poland',
+    },
+    {
+      name: 'Asma Suites',
+      city: 'Santorini',
+      country: 'Greece',
+    },
+    {
+      name: 'Cityden Apartments',
+      city: 'Amsterdam',
+      country: 'Netherlands',
+    },
+    {
+      name: 'Mandarin Oriental',
+      city: 'Miami',
+      country: 'USA',
+    },
+    {
+      name: 'Concept Terrace Hotel',
+      city: 'Rome',
+      country: 'Italy',
+    },
+    {
+      name: 'Ponta Mar Hotel',
+      city: 'Fortaleza',
+      country: 'Brazil',
+    },
+    {
+      name: 'Four Seasons Hotel',
+      city: 'Sydney',
+      country: 'Australia',
+    },
+    {
+      name: 'Le Meridien',
+      city: 'Nice',
+      country: 'France',
+    },
+    {
+      name: 'Apart Neptun',
+      city: 'Gdansk',
+      country: 'Poland',
+    },
+    {
+      name: 'Lux Isla',
+      city: 'Ibiza',
+      country: 'Spain',
+    },
+    {
+      name: 'Nox Hostel',
+      city: 'London',
+      country: 'UK',
+    },
+    {
+      name: 'Leonardo Vienna',
+      city: 'Vienna',
+      country: 'Austria',
+    },
+    {
+      name: 'Adagio Aparthotel',
+      city: 'Edinburgh',
+      country: 'UK',
+    },
+    {
+      name: 'Steigenberger Hotel',
+      city: 'Hamburg',
+      country: 'Germany',
+    },
+  ];
 
 
-
-const data = [
-    {
-        country: 'Russia',
-        city: 'Saint Petersburg',
-        hotel: 'Hotel Leopold',
-    },
-    {
-        country: 'Spain',
-        city: 'Santa Cruz de Tenerife',
-        hotel: 'Apartment Sunshine',
-    },
-    {
-        country: 'Slowakia',
-        city: 'Vysokie Tatry',
-        hotel: 'Villa Kunerad',
-    },
-    {
-        country: 'Germany',
-        city: 'Berlin',
-        hotel: 'Hostel Friendship',
-    },
-    {
-        country: 'Indonesia',
-        city: 'Bali',
-        hotel: 'Ubud Bali Resort&SPA',
-    },
-    {
-        country: 'Netherlands',
-        city: 'Rotterdam',
-        hotel: 'King Kong Hostel',
-    },
-    {
-        country: 'Marocco',
-        city: 'Ourika',
-        hotel: 'Rokoko Hotel',
-    },
-    {
-        country: 'Germany',
-        city: 'Berlin',
-        hotel: 'Hotel Rehberge Berlin Mitte',
-    },
-];
+function searchObjects (str){
+    const bustingHotels = hotels.filter((item) => item.name === str || item.city === str || item.country === str);
+    let result = bustingHotels.reduce((acc, item) => {
+      acc += `${item.name}, ${item.city}, ${item.country}  `;
+      return acc;
+    }, "");
+    return result.split("  ").slice(0, -1);
+  };
 
 
-function searchObjects( str){
-    const result=[];
-    for (let i=0;i<data.length;i++){
-        if(data[i].country==str || data[i].city==str || data[i].hotel==str ){
-            const data_new  = data[i].country +','+data[i].city +','+data[i].hotel;
-            result.push(data_new);
+function countryCity(arr){
+    const result = {};
+    arr.forEach((item) => {
+        if (result.hasOwnProperty(item.country)) {
+            result[item.country].push(item.city);
+        } else {
+            result[item.country] = [item.city];
         }
-    }
-    return(result); 
+      });
+      return(result)
 }
+
+
+  function getCalendarMonth(daysInMonth, daysInWeek, dayOfWeek){
+    if (dayOfWeek > daysInWeek){ 
+        return false;
+    } 
+
+    const arrMonth = [];
+    for (let i = 0; i < daysInMonth; i++) {
+      arrMonth[i] = i + 1;
+    }
+  
+    let nextMonth;
+    if((daysInMonth + dayOfWeek) % daysInWeek == 0 ){
+        nextMonth = 0;
+    }
+    else{
+        nextMonth = daysInWeek - ((daysInMonth + dayOfWeek) % daysInWeek);
+    }
+
+    const arrCalendarMonth = [...arrMonth.slice(-dayOfWeek), ...arrMonth, ...arrMonth.slice(0, nextMonth)];
+  
+    const result = [];
+    for (let i = 0; i < arrCalendarMonth.length; i += daysInWeek) {
+        let item = arrCalendarMonth.slice(i,i+daysInWeek);
+        result.push(item);
+    }
+  
+    return(result);
+  };
+
+
